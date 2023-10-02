a9e2cf1ae7f0c5439f9a18e495fbb324040089a5
CAbdoulaye <c_faye@mail.fhsu.edu>
2023-10-02 18:22:08 -0500

    Added csv file to project

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name" : ["john", "robert", "angel"],
+        "Age": [19, 32, 21],
+        "Origin": ['France', "US", "UK"]
+    }
+)
+
+#print(df)
+print()
+#print(list(df.columns))
+#print(df["Origin"])
+#print(df["Age"])
+
+#weight = pd.Series([180, 165, 230], name="weight")
+print(df)
