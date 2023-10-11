3d108deb9b3850846e9e8b81896c18eee34e5898
CAbdoulaye <c_faye@mail.fhsu.edu>
2023-10-10 21:56:05 -0500

    edited graphs on main. Updated read me

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 #### Mini Project 2
 
 ## Description
-This project will be using Pandas and Faker to create dataframes. This isn't intended to be full blown data science project. The goal here is to come up with some question and then see what API or datasets you can use to get the information needed to answer that question. 
+This project will be using Pandasto create dataframes. This isn't intended to be full blown data science project. The goal here is to compare the hours of sleep, qualityt of sleep, and sleep schedule of individuals with different occupations
 ## Pip install instructions
 Please run the following:
 ```
--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-#(5/5 points) Pr
-# (5/5 points) Proper import of packages used.
-# (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
-#       Think of some question you would like to solve such as:
-#       "How many homes in the US have access to 100Mbps Internet or more?"
-#       "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
-#       Here are some other great datasets: https://www.kaggle.com/datasets
-# (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
-# (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
-# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-# (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
-# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
-
 # INF601 - Advanced Programming in Python
 # Cheikh Abdoulaye Faye
 # Mini Project 2
@@ -21,7 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 
-
 def getInfoList(occupationsList):
   # return list of all occupation dataframe descriptions
   allOccupationsInfo = []
@@ -29,7 +14,6 @@ def getInfoList(occupationsList):
     occupationInfo = getInfo(element)
     allOccupationsInfo.append(occupationInfo)
   return allOccupationsInfo
-
 
 def getInfo(occupationName):
   # return dictionary of occupation dataframe description
@@ -87,7 +71,6 @@ def getInfo(occupationName):
   infoDictionary['stressLevelMax'] = stressLevelMax
   return infoDictionary
 
-
 def getOccupationsList(data):
   # return a list with each occupation as a dataframe
   accountants = data.loc[data['Occupation'] == 'Accountant']
@@ -100,17 +83,25 @@ def getOccupationsList(data):
   scientists = data.loc[data['Occupation'] == 'Scientist']
   teachers = data.loc[data['Occupation'] == 'Teacher']
 
-  allOccupations = [accountants, doctors, engineers, lawyers, nurses, managers, salespersons, scientists, teachers]
+  allOccupations = [
+      accountants, doctors, engineers, lawyers, nurses, managers, salespersons,
+      scientists, teachers
+  ]
   return allOccupations
 
 
 def sortOccupations(listOfOccupations):
   # sort dataframe to only get occupation, ages, ... ignore everything else I will not use
   for i in range(len(listOfOccupations)):
-    listOfOccupations[i] = (listOfOccupations[i])[[ 'Occupation', 'Age', 'Sleep Duration', 'Quality of Sleep', 'Stress Level']]
+    listOfOccupations[i] = (listOfOccupations[i])[[
+        'Occupation', 'Age', 'Sleep Duration', 'Quality of Sleep',
+        'Stress Level'
+    ]]
   return listOfOccupations
 
+
 def getListOfMeans(descriptiveList):
+  # return a list with only means
   newList = []
   newDict = {}
   for element in descriptiveList:
@@ -123,44 +114,70 @@ def getListOfMeans(descriptiveList):
   return newList
 
 
-def makeIndividualGraph(descriptions, name):
+def makeAllGraphs(descriptions):
+  # make individual graphs for each field and one with all together
+  makeIndividualGraph(descriptions, 'sleepDurationMean',
+                      'Hours of Sleep per Occupation', 'Hours')
+  makeIndividualGraph(descriptions, 'qualityOfSleepMean',
+                      'Quality of Sleep per Occupation', 'PSQI')
+  makeIndividualGraph(descriptions, 'stressLevelMean',
+                      'Stress Level per Occupation', 'HRV')
+  finalGraph(descriptions)
+
+def makeIndividualGraph(descriptions, name, title, ylabel):
+  # Graph each bar individully
   values = []
   keys = []
   for element in descriptions:
     values.append(element[name])
     keys.append(element['Occupation'])
-  length = len(descriptions)
-  # plt.bar(range(length), values, tick_label=keys)
   plt.bar(keys, values)
   addLabels(values)
-
-  plt.title(name)
-
+  plt.ylabel(ylabel)
+  plt.xlabel('Occupations')
+  plt.title(title)
   # Save graphs to png files
   fileName = "Charts/" + name + ".png"
   plt.savefig(fileName)
   plt.show()
 
+
 def addLabels(yValues):
+  # add y value on each bar
   for i in range(len(yValues)):
     plt.text(i, (yValues[i] + 0.1), yValues[i])
 
+
 def finalGraph(data):
+  # Graph all bars on the same y-x plan
   listOfList = []
   keys = []
   values = []
   x = len(data)
   x = np.arange(x)
   names = ["sleepDurationMean", 'qualityOfSleepMean', 'stressLevelMean']
+  attributes = ['hoursofsleep', 'qualityofsleep', 'stresslevel']
   colors = ['b', 'g', 'r']
   for i in range(3):
     for element in data:
-      if i == 0:
+      if i == 1:
         keys.append(element['Occupation'])
       values.append(element[names[i]])
     listOfList.append(values)
-    plt.bar((x + (0.25 * (i + 1))) * 2, listOfList[0], color=colors[i], width=0.50)
+    plt.bar((x + (0.25 * (i + 1))) * 2,
+            listOfList[0],
+            color=colors[i],
+            width=0.50,
+            label=attributes[i])
+    plt.xticks((x + 0.5) * 2, keys)
     values.clear()
+  plt.legend(loc='upper right', ncols=3)
+  plt.ylim(0, 9)
+  plt.grid(color='grey', linestyle=':', linewidth=1.0, axis='y', alpha=0.2)
+  plt.xlabel('Occupations')
+  plt.title('Hours of Sleep vs Quality of Sleep vs Stress Level')
+  fileName = "Charts/CombinedGraph.png"
+  plt.savefig(fileName)
   plt.show()
 
 
@@ -173,8 +190,6 @@ except:
 dataframe = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv',
                         index_col='Person ID')
 
-# changeOccupationsNames(dataframe)
-
 # sorted dataframe by occupation
 dataframe = dataframe.sort_values(by=['Occupation'])
 
@@ -184,7 +199,4 @@ sortedOccupations = sortOccupations(occupations)
 
 descriptionList = getInfoList(sortedOccupations)
 
-makeIndividualGraph(descriptionList, 'sleepDurationMean')
-makeIndividualGraph(descriptionList, 'qualityOfSleepMean')
-makeIndividualGraph(descriptionList, 'stressLevelMean')
-finalGraph(descriptionList)
+makeAllGraphs(descriptionList)
